c7715241642ce1dad1f9af551c5cf4f94202a4dd
Tarun Raheja <trolldemort9@gmail.com>
2019-03-08 11:37:56 -0500

    fixed commenting

--- task-1/custommerge.py
+++ task-1/custommerge.py
@@ -1,4 +1,11 @@
+# SOLUTION TO TASK 1B
+# GSoC 2019
+# AUTHOR - TARUN RAHEJA
+#SCRIPT FOR CUSTOM MERGER
+
 import os
+
+#custom merge function, reads input files, sums up numbers, prints to another file
 def merge(file_list,output_file):
     f_out = file(output_file,'w')
     sum = 0
@@ -7,10 +14,5 @@ def merge(file_list,output_file):
         num = int(f_in.read())
         sum += num
         f_in.close()
-    save-path = "~/Downloads/"
-    fname = "hello"
-    fullname = os.path.join(save-path + fname)
-    f1 = open(fullname, "w")
-    f1.write(sum)
-    f1.close()
+    f_out.write(sum)
     f_out.close()
--- task-1/ganga-job.py
+++ task-1/ganga-job.py
@@ -1,17 +1,31 @@
+# SOLUTION TO TASK 1B
+# GSoC 2019
+# AUTHOR - TARUN RAHEJA
+
 import os
 
+#starts new job
 job = Job()
+#specifies executable to run on Grid
 job.application = Executable()
 job.application.exe = File("count-word.sh")
+#specifies pagewise arguments to the executable
 args = [["page0.pdf"],["page1.pdf"],["page2.pdf"],["page3.pdf"],["page4.pdf"],["page5.pdf"],["page6.pdf"],["page7.pdf"],["page8.pdf"],["page9.pdf"],["page10.pdf"],["page11.pdf"]]
+#splits the job
 splitter = ArgSplitter(args=args)
+#creates list of file names
 filelist = []
 for i in range(len(args)):
 	filename = args[i][0]
 	filelist.append(filename)
+#assigns filelist to job applicatio arguments
 job.application.args = filelist
+#assigns splitter to job application
 job.splitter = splitter
+#assigns filelist to inputfiles
 job.inputfiles = filelist
+#specifies backend
 job.backend = "Local"
+#specifier custom merger
 job.postprocessors = CustomMerger(module="./custommerge.py")
 job.submit()
